ebeabc3bc0a80d30f29ce7f6a0d7094cbec0c2b4
bapolo24 <byronjoseapoloaguilar@gmail.com>
2026-08-25 20:38:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 import streamlit as st
+import pandas as pd
+st.title("bootcamps data analytics for oil& gas")
+
+pozo="spe-001"
+petroleo_bpd=1250
+agua_bpd=350.50
+status= True
+liquido_total_bpd=petroleo_bpd+agua_bpd
+corte_agua_pct=(agua_bpd)/(liquido_total_bpd)*100
+st.write(pozo)
+st.write(petroleo_bpd)
+st.write(agua_bpd)
+st.write(liquido_total_bpd)
+st.write(corte_agua_pct)
