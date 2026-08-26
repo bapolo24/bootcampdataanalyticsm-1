deacd089bc2af5d228418568373e596f9c8bf73e
bapolo24 <byronjoseapoloaguilar@gmail.com>
2026-08-26 00:09:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,5 @@ presion_yacimiento= st.number_input("ingrese la presion de yacimiento", min_valu
 presion_fondo=st.number_input("ingrese la presion de fondo", min_value=0, max_value=9000, value=1500)
 decimales=st.slider("seleccione la cantidad de decimales para el resultado",min_value=0, max_value=4, value=2)
 
-caudal=calcular_caudal_vogel(caudal_maximo, presion_yacimiento, presion_fondo, decimales)
 
-st.write("el caudal es:", caudal)
 
