34a30d0babf752d6b915d204e9ffef3d4e1e2b19
bapolo24 <byronjoseapoloaguilar@gmail.com>
2026-08-26 16:50:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 from funciones_calculo import (calcular_liquido,calcular_bsw,calcular_gor)
 from funciones_datos import (filtrar_pozo,resumen_dataframe)
-from clases_pozo import Pozo
+from clases_pozo import Pozo, campo
 
 
 
