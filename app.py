e04bfe0c3af62ff674cb47b8cec350d57532b2f3
bapolo24 <byronjoseapoloaguilar@gmail.com>
2026-08-26 17:15:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 from funciones_calculo import (calcular_liquido,calcular_bsw,calcular_gor, proyectar_produccion)
 from funciones_datos import (filtrar_pozo,resumen_dataframe)
 from clases_pozo import Pozo, Campo
-
+from clases_campo import Campo
 
 
 st.title("bootcamps data analytics for oil& gas")
